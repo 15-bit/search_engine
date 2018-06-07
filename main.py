b1299aa26697ce29d49b638eae953c246a5be9ac
15-bit <a-sergeyev@inbox.ru>
2018-06-08 09:23:28 +1100

    init

---
+++ main.py
@@ -0,0 +1,59 @@
+def get_page(url):
+    try:
+        import urllib.request
+        return urllib.request.urlopen(url).read().decode()
+    except:
+        return ''
+
+def union(list1, list2):
+    for e in list2:
+        if e not in list1:
+            list1.append(e)
+
+def get_next_target(page):
+    start_link = page.find('<a href=')
+    if start_link == -1: return None, 0
+    else:
+        start_quote = page.find('"', start_link)
+        end_quote = page.find('"', start_quote + 1)
+        url = page[start_quote + 1: end_quote]
+
+        return url, end_quote
+
+def print_all_links(page):
+    while True:
+        url, endpos = get_next_target(page)
+        if url:
+            print(url)
+            page = page[endpos:]
+        else:
+            break
+
+def get_all_links(page):
+    links = []
+    while True:
+        url, endpos = get_next_target(page)
+        if url:
+            if url[:4] == 'http' or url[:3] == 'www':
+                links.append(url)
+            page = page[endpos:]
+        else:
+            return links
+
+def crawl_web(page):
+    tocrawl = [page]
+    crawled = []
+
+    while tocrawl:
+        page = tocrawl.pop()
+        if page not in crawled:
+            union(tocrawl, get_all_links(get_page(page)))
+            crawled.append(page)
+            print(page)
+    return crawled
+
+seed =('https://xkcd.com/353/')
+#seed =('https://yandex.ru')
+
+print(crawl_web(seed))
+
